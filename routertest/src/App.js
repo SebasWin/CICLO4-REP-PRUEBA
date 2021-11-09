6119ae3368e7f96845b36af2d9f0bc4b73a69f2c
SebasWin <sebastian200431@gmail.com>
2021-11-09 13:03:35 -0500

    iniciado commit

---
+++ routertest/src/App.js
@@ -0,0 +1,50 @@
+import logo from './logo.svg';
+import './App.css';
+import { Link, Route, Switch } from "react-router-dom";
+
+const Home = () => (
+  <div>
+  <h2>Home</h2>
+  </div>
+ );
+ const Category = () => (
+  <div>
+  <h2>Category</h2>
+  </div>
+ );
+ const Products = () => (
+  <div>
+  <h2>Products</h2>
+  </div>
+
+function App() {
+  return (
+    <div>
+      <nav className="navbar navbar-light">
+      <ul className="nav navbar-nav">
+      <li>
+      <Link to="/">Home</Link>
+      </li>
+      <li>
+      <Link to="/category">Category</Link>
+      </li>
+      <li>
+      <Link to="/products">Products</Link>
+      </li>
+      </ul>
+      </nav>
+      {/* Los componentes de la ruta se representan si la propiedad de la ruta coincide con la URL actual */}
+      <Route path="/">
+        <Home />
+      </Route>
+      <Route path="/category">
+        <Category />
+      </Route>
+      <Route path="/products">
+       <Products />
+      </Route>
+      </div>
+  );
+}
+
+export default App;
